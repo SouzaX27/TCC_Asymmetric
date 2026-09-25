647dd505529e7239526dd4238150333cf943ba0e
SouzaX27 <g.souzacardoso27@gmail.com>
2026-09-25 10:50:03 -0300

    refactor: Atualização na Validação dos campos de Registro

--- backend/Asymmetric/urls.py
+++ backend/Asymmetric/urls.py
@@ -28,9 +28,6 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/', include('store.urls')),
 
-    # Rotas autenticação JWT
-    path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'), # endpoint login
-    path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'), # renovação token
 ]
 
 if settings.DEBUG:
